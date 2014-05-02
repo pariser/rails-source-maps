f214142ab67e9be17b4e0496c0ad7d777c930f38
Andrew Pariser <pariser@gmail.com>
2014-05-01 19:40:30 -0700

    Start of project.

---
+++ README.md
@@ -0,0 +1,9 @@
+A utility to create source maps as part of the Rails asset pipeline.
+
+Install:
+
+    $ npm install -g rails-source-maps
+
+Example (from project root):
+
+    $ rails-source-maps example/example.js
---
+++ example/example.js
@@ -0,0 +1,36 @@
+var Example = (function() {
+  "use strict";
+
+  var example = {};
+
+  example.VARIABLE = [
+    'foo',
+    'bar',
+    'baz'
+  ];
+
+  example.fn = function(arg, cb) {
+    if (typeof cb === 'undefined') {
+      cb = arg;
+      arg = null;
+    }
+
+    if (!arg) {
+      arg = '[defaultArgument]';
+    }
+
+    console.log('Got argument: ' + arg);
+
+    if (cb) {
+      cb();
+    }
+  };
+
+  return example;
+
+}());
+
+console.log('Example started.');
+Example.fn(Example.VARIABLE, function() {
+  console.log('Example finished.');
+});
